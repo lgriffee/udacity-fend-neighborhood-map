05e356eb2ddc0ee1fabc9ab80c9b26eca25bd665
Laura Griffee <laura@mailzone.com>
2018-09-29 13:19:52 -0600

    feat: Set up basic page layout for UI

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from '../icons/logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <body>
+        <header>
+          <nav className="top-nav">
+            <h1>LANDER</h1>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        <main>
+          <section className="map" >
+            <h2>Map</h2>
+          </section>
+
+          <section className="list-view">
+            <h2>List View</h2>
+          </section>
+        </main>
+      </body>
     );
   }
 }
